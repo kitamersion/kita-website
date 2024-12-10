3a1582df0f94d833827d6e1693f90f336e7cce23
HelpfulFish <learning_open_source_dev@proton.me>
2024-12-10 20:57:41 +1300

    feat: update landing page + tailwind + eslint

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,7 +1,7 @@
 import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
-import tailwindPlugin from "./plugins/tailwind-config.cjs";
+import tailwindPlugin from "./plugins/tailwind-config.js";
 
 const config: Config = {
   title: "Kitamersion",
